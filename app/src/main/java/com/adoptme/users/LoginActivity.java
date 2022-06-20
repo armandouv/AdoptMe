e326e0ff6c717086ed8b5391dd0c9ca638a24b24
armandouv <armandouv@fb.com>
2022-06-20 13:44:27 -0700

    Implement login

---
+++ app/src/main/java/com/adoptme/users/LoginActivity.java
@@ -0,0 +1,58 @@
+package com.adoptme.users;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.adoptme.databinding.ActivityLoginBinding;
+import com.parse.ParseUser;
+
+/**
+ * Allows a User to log in.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding mBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mBinding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
+        if (ParseUser.getCurrentUser() != null) {
+            // TODO: Go to TimelineActivity
+        }
+
+        mBinding.loginButton.setOnClickListener((view) -> {
+            Editable username = mBinding.username.getText();
+            Editable password = mBinding.password.getText();
+            // TODO: Check for null values
+
+            loginUser(username.toString(), password.toString());
+
+            username.clear();
+            password.clear();
+        });
+
+        mBinding.goSignupButton.setOnClickListener(v -> {
+            // TODO: Go to sign up activity
+        });
+    }
+
+    private void loginUser(String username, String password) {
+        ParseUser.logInInBackground(username, password, (user, e) -> {
+            if (e != null) {
+                Toast.makeText(this, "Couldn't log in", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            Toast.makeText(this, "Success!", Toast.LENGTH_SHORT).show();
+            // TODO: Go to timeline activity.
+            finish();
+        });
+    }
+}
